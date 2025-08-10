112ae2c35bdfe100aa476534a35519cece141a0c
Kevin Tran <kevinohappyface@gmail.com>
2025-08-10 19:19:44 +1000

    Disable saving/loading PolygonFile objects
    I don't think the code ever does this anyway.

--- polygon_cli/json_encoders.py
+++ polygon_cli/json_encoders.py
@@ -4,6 +4,9 @@ from .polygon_file import PolygonFile
 
 def my_json_encoder(obj):
     if isinstance(obj, PolygonFile):
+        # TODO: I don't think this should be possible, so I'm going
+        # to assert false here
+        raise TypeError("This should not be able to be a PolygonFile")
         res = obj.__dict__
         res.update({'__type': 'PolygonFile'})
         return res
@@ -18,6 +21,9 @@ def my_json_decoder(dct):
     if '__type' not in dct:
         return dct
     if dct['__type'] == 'PolygonFile':
+        # TODO: I don't think this should be possible, so I'm going
+        # to assert false here
+        raise TypeError("This should not be able to be a PolygonFile")
         res = PolygonFile()
         res.by_dict(dct)
         return res
